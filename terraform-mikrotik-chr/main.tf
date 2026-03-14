fa7a274d8f121ec2bb568ac619ebbfb7e3b5e873
Ridho <ridhofauza799@gmail.com>
2026-03-15 03:08:59 +0700

    modify terraform mikrotik lab

--- terraform-mikrotik-chr/main.tf
+++ terraform-mikrotik-chr/main.tf
@@ -65,7 +65,42 @@ resource "aws_route_table_association" "public_zone1" {
 }
 
 ############################
-# Security Group
+# Security Group 1
+############################
+resource "aws_security_group" "server_lab" {
+  name        = "server_lab_sg"
+  description = "Allow ping and http traffic"
+  vpc_id      = aws_vpc.mikrotik_vpc.id
+
+  tags = {
+    Name = "server_lab_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp_ping" {
+  security_group_id = aws_security_group.server_lab.id
+  cidr_ipv4         = "11.0.0.0/16"
+  ip_protocol       = "icmp"
+  from_port         = -1
+  to_port           = -1
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.server_lab.id
+  cidr_ipv4         = "11.0.0.0/16"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_1_ipv4" {
+  security_group_id = aws_security_group.server_lab.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+############################
+# Security Group 2
 ############################
 resource "aws_security_group" "mikrotik_lab" {
   name        = "mikrotik_lab"
@@ -77,7 +112,6 @@ resource "aws_security_group" "mikrotik_lab" {
   }
 }
 
-/*
 resource "aws_vpc_security_group_ingress_rule" "allow_winbox" {
   security_group_id = aws_security_group.mikrotik_lab.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -100,12 +134,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ipsec_2" {
   ip_protocol       = "udp"
   from_port         = 4500
   to_port           = 4500
-}*/
+}
 
-resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound_ipv4" {
-  security_group_id = aws_security_group.mikrotik_lab.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+resource "aws_vpc_security_group_ingress_rule" "sg_ingress_server" {
+  security_group_id            = aws_security_group.mikrotik_lab.id
+  referenced_security_group_id = aws_security_group.server_lab.id
+  ip_protocol                  = "-1"
+  from_port                    = 0
+  to_port                      = 0
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
@@ -115,7 +151,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
 }
 
 ############################
-# EC2 Instance
+# EC2 Instance Mikrotik
 ############################
 resource "aws_instance" "mikrotik_chr_instance" {
   ami                         = "ami-0989af5bf0021a8ae"
@@ -144,4 +180,28 @@ resource "aws_eip" "mikrotik_eip" {
 resource "aws_eip_association" "mikrotik_eip_assoc" {
   instance_id   = aws_instance.mikrotik_chr_instance.id
   allocation_id = aws_eip.mikrotik_eip.id
+}
+
+############################
+# EC2 Instance Web Server
+############################
+resource "aws_instance" "web_server_instance" {
+  ami                         = "ami-0f3caa1cf4417e51b" # us-east-1
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_mikrotik.id
+  vpc_security_group_ids      = [aws_security_group.server_lab.id]
+  associate_public_ip_address = true
+  source_dest_check           = false
+  user_data                   = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Web Server - Mikrotik Lab"
+  }
 }
